6803194fd65367d78fc396262519230190950094
David Crosby <dcrosby42@gmail.com>
2020-11-10 08:31:52 -0500

    Update local dev helper script

--- project.sh
+++ project.sh
@@ -6,6 +6,7 @@ mminstall() {
   cp build/ng-tags-input.min.js ${MM_ASSETS}/javascripts/ng-tags-input.js
   cp build/ng-tags-input.min.css ${MM_ASSETS}/stylesheets/ng-tags-input.css
 }
+echo "mminstall: rebuild, and copy js and css into $MM_ASSETS"
 
 mminstalldev() {
   set -x
@@ -13,3 +14,4 @@ mminstalldev() {
   cp build/ng-tags-input.js ${MM_ASSETS}/javascripts/ng-tags-input.js
   cp build/ng-tags-input.css ${MM_ASSETS}/stylesheets/ng-tags-input.css
 }
+echo "mminstalldev: rebuild (without minimizing), and copy js and css into $MM_ASSETS"
